2675fbcabd3ff085e1e5c536c8f78dd8fc4a60c0
Felipe Ferreira de Oliveira <felipeferreira_811@hotmail.com>
2019-10-04 09:01:17 -0300

    Removendo pasta e arquivos

--- algamoneyfel-api/src/main/java/com/algamoneyfel/api/model/TipoLancamento.java
+++
@@ -1,8 +0,0 @@
-package com.algamoneyfel.api.model;
-
-public enum TipoLancamento {
-	
-	RECEITA,
-	DESPESA
-
-}
--- algamoneyfel-api/src/main/java/com/algamoneyfel/api/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.algamoneyfel.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.algamoneyfel.api.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-	
-}
