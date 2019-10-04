585a914f0337ec3e3b4bb473966928a10a7ddbd7
Felipe Ferreira de Oliveira <felipeferreira_811@hotmail.com>
2019-10-04 11:26:15 -0300

    Revomendo...

--- src/main/java/com/algamoneyfel/api/service/exception/pessoaInexistenteOuInativaException.java
+++
@@ -1,7 +0,0 @@
-package com.algamoneyfel.api.service.exception;
-
-public class pessoaInexistenteOuInativaException extends RuntimeException {
-	
-	private static final long seriaVersionUID = 1L;
-
-}
